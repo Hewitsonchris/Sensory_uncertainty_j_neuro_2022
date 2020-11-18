0206c2a0ffcb61951f397a2917b3f84b56c2a086
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2020-11-18 18:10:48 +1100

    removing old files

--- code/inspect_trajectory.py
+++ code/inspect_trajectory.py
@@ -5,31 +5,68 @@ import matplotlib.pyplot as plt
 import datatable as dt
 from scipy.interpolate import interp1d
 
-fn = '../data/G345_adaptation.csv'
-
+fn = '../datta/G13.csv'
 d = dt.fread(fn)
+d.names = ('group', 'subject', 'trial', 'sig_mp', 'sig_ep', 'x', 'y')
 
-dd = d.to_numpy()
-dd = np.reshape(dd, (-1, 2))
-
-dd = dt.Frame(dd)
-dd.names = ('x', 'y')
-dd['trial'] = np.tile(np.arange(0, 1080, 1), dd.shape[0] // 1080)
-dd['subject'] = np.repeat(np.arange(1, 21, 1), dd.shape[0] // 20)
+d = d.to_pandas()
 
 samp_rate = 0.01
-ns = 200
+ns = 100
 
-for i in range(0, d.shape[1], 2):
 
-    x = d[:, i].to_numpy()[:, 0]
-    y = d[:, i + 1].to_numpy()[:, 0]
+def interp_traj(d):
 
+    group = d['group'].to_numpy()[0]
+    subject = d['subject'].to_numpy()[0]
+    trial = d['trial'].to_numpy()[0]
+    sig_mp = d['sig_mp'].to_numpy()[0]
+    sig_ep = d['sig_ep'].to_numpy()[0]
+    x = d['x'].to_numpy()
+    y = d['y'].to_numpy()
+    x = x[~np.isnan(x)]
+    y = y[~np.isnan(y)]
     t = np.linspace(0, x.size * samp_rate, x.size)
     ts = np.linspace(t[0], t[-1], ns)
-
+    t_ind = np.arange(0, ts.size, 1)
     fx = interp1d(t, x, kind='cubic')
     xs = fx(ts)
-
     fy = interp1d(t, y, kind='cubic')
     ys = fy(ts)
+    d = pd.DataFrame({
+        'group': group,
+        'subject': subject,
+        'trial': trial,
+        'sig_mp': sig_mp,
+        'sig_ep': sig_ep,
+        'x': xs,
+        'y': ys,
+        't': ts,
+        't_ind': t_ind
+    })
+    return d
+
+
+ds = d.groupby(['group', 'subject', 'trial']).apply(interp_traj)
+ds.reset_index(inplace=True, drop=True)
+
+dds = ds.groupby(['sig_mp', 'sig_ep', 't_ind'])['x', 'y', 't'].mean()
+dds = dds.reset_index()
+dds = dds.sort_values(['sig_mp', 'sig_ep', 't_ind'])
+
+# nrow = dds['sig_mp'].unique().size
+# ncol = dds['sig_ep'].unique().size
+# fig, ax = plt.subplots(nrow, ncol)
+# for i, smp in enumerate(dds['sig_mp'].unique()):
+#     for j, sep in enumerate(dds['sig_ep'].unique()):
+#         dd = dds[(dds['sig_mp'] == smp) & (dds['sig_ep'] == sep)]
+#         sns.scatterplot(data=dd, x='x', y='y', hue='sig_mp', ax=ax[i, j])
+
+sns.scatterplot(data=dds,
+                x='x',
+                y='y',
+                hue='sig_mp',
+                style='sig_ep',
+                palette='deep',
+                s=100)
+plt.show()
